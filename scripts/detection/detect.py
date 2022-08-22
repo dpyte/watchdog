af280ec4d0dbdec660477d841a4ca0e6842f0f31
Dhruv Patel <pateldrv456@gmail.com>
2022-08-22 12:48:33 -0400

    no change

--- scripts/detection/detect.py
+++ scripts/detection/detect.py
@@ -1,30 +1,27 @@
 #!/bin/env python
 
-import re
-import subprocess as sp
-import xml.etree.ElementTree as et
+import os
+import sys
+import fcntl
+import socket
+import struct
 
 
-def query_devices():
-	device_re = re.compile(b"Bus\s+(?P<bus>\d+)\s+Device\s+(?P<device>\d+).+ID\s(?P<id>\w+:\w+)\s(?P<tag>.+)$", re.I)
-	df = sp.check_output('lsusb')
-	devices = []
-	for x in df.split(b'\n'):
-		if x:
-			info = device_re.match(x)
-			if info:
-				d_info = info.groupdict()
-				d_info['device'] = '/dev/bus/usb/%s/%s' % (d_info.pop('bus'), d_info.pop('device'))
-				devices.append(d_info)
-	return devices
+def get_ip_address(ifname):
+	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+	return socket.inet_ntoa(
+		fcntl.ioctl(
+			sock.fileno(),
+			0x8195,
+			struct.pack('256s', bytes(ifname[:15], 'utf-8'))
+		)
+		[20:24]
+	)
 
 
 def main():
-	devs = query_devices()
-	xdoc = et.parse('/var/system/openads/config/camera/info/info.xml')
-
+	ip_addrs = get_ip_address('en0')
 
 
 if __name__ == '__main__':
 	main()
-
