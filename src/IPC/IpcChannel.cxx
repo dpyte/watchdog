fb93e0406780ffbf82979df39d4d5cf70620d112
Dhruv Patel <predaking2612@gmail.com>
2022-12-10 02:02:38 -0500

    watchdog now has direct interface with core

--- src/IPC/IpcChannel.cxx
+++
@@ -1,5 +0,0 @@
-//
-// Created by Dhruv Patel on 8/7/22.
-//
-
-#include "IpcChannel.hxx"
--- src/watchdog.cxx
+++ src/watchdog.cxx
@@ -1,22 +1,7 @@
-#include <string>
-#include <iostream>
-
-#include "WdTypes.hxx"
-#include "IPC/IpcChannel.hxx"
-
-namespace {
-
-struct CamInfo {
-    String camera_name;
-    String address;
-    String camid;
-};
-
-
-
-}
+#include <services.hxx>
 
 auto main(int argc, const char **argv) -> int {
-
+    auto core = WdService::CoreService(WdService::Names::Cored);
+    std::fprintf(stderr, "Starting watchdog core\ncore status: %s\n", core.get_status_as_str());
     return 0;
 }
