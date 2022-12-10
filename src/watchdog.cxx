ddb7e87138e6b99d0bd5e7b3ed1358b73d337fa1
Dhruv Patel <predaking2612@gmail.com>
2022-12-10 15:34:49 -0500

    conform to updated api

--- src/watchdog.cxx
+++ src/watchdog.cxx
@@ -1,8 +1,22 @@
+#include <string_view>
 #include <services.hxx>
 #include <spdlog/spdlog.h>
 
+/// Kick-starts the system
 auto main(int argc, const char **argv) -> int {
-    spdlog::warn("Starting watchdog core");
-    auto core = WdService::CoreService(WdService::Names::Cored);
-    return 0;
+    // Stop wasting resources you imbecile
+    if (argc <= 1) {
+        spdlog::warn("program called without any arguments. quitting ...");
+        return -1;
+    }
+
+    i32 p_return_status = -1;
+    if (argv[1] == std::string_view("--start")) {
+        spdlog::warn("Starting watchdog core");
+
+        auto core = WdService::CoreService(WdService::Names::Cored);
+        core.activate();
+        p_return_status = core.get_exit_status();
+    }
+    return p_return_status;
 }
